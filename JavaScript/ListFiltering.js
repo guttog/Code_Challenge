f04f9d94b654ef96cc4c4e67fed89f7fe4aeac53
guttog <cesaraugusto.gaiotto@gmail.com>
2024-02-06 10:40:47 -0300

    adicionando o readline-sync (input pro js)

--- JavaScript/ListFiltering.js
+++ JavaScript/ListFiltering.js
@@ -1,1 +1,9 @@
-console.log("teste");
+if (typeof x == 'number'){
+    console.log("É Número!")
+}
+else if (typeof x == 'string'){
+    console.log("É String!")
+}
+else{
+    console.log("Não é nada!")
+}
