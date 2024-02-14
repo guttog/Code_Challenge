bf1631969163dfb1dff2e305e2528a1b75359e8f
CESAR AUGUSTO ALVES GAIOTTO <Z331982@nt.embratel.com.br>
2024-02-14 08:40:04 -0300

    atualizando 14/02

--- JavaScript/teste.js
+++ JavaScript/teste.js
@@ -1,5 +1,13 @@
-let arr1 = [1,2,34,54,6,7,8,9,0];
+import input from 'readline-sync'
 
-for(let dados in arr1){
-    console.log(arr1[dados]);
+var arr = ['banana','abacaxi','laranja'];
+console.log("Escolha um item: 1)"+arr[0]+", 2)"+arr[1]+", 3)" +arr[2]+ ", 4) Sair")
+
+var num = input.question("Digite um numero: ");
+
+while(num != 4){
+    
+        var item_selecionado = arr[num-1]
+        console.log(item_selecionado)
+        continue;
 };
