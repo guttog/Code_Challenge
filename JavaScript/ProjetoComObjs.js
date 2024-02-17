e3690b35e814bdc3b6d522eaee3cc96b826a4a08
César Gaiotto <cesaraugusto.gaiotto@gmail.com>
2024-02-17 15:01:09 -0300

    adicinando uma opção e melhorando

--- JavaScript/ProjetoComObjs.js
+++ JavaScript/ProjetoComObjs.js
@@ -7,42 +7,55 @@ var arr =[
     {id: 4, nome: "Gaiotto", idade: 80},
     {id: 5, nome: "Alves", idade: 53}
 ];
-   
+   function mostrarArr(){
+    for(let dados_impressos in arr){
+        console.log(arr[dados_impressos]);
+        };
+    };
   
-    let mudar_arr = input.question("Qual valor voce deseja alterar? (3)Sair (1)Idade ou (2)Nome: "); //input para escolher ID ou Nome
-                    if (Number(mudar_arr) == 1){
+    let escolha = input.question("Oque deseja fazer? (4)Sair (1)Mudar Idade (2)Mudar Nome ou (3)Adicinar Nome/Idade: "); //input para escolher ID ou Nome
+                    if (Number(escolha) == 1){
                     
-                    console.log(arr)
-                    mudar_arr = input.question("\nEscolha o ID da idade voce deseja alterar?: "); //input para escolher qual Idade vai alterar
+                    mostrarArr();
+                    escolha = input.question("\nEscolha o ID da idade voce deseja alterar?: "); //input para escolher qual Idade vai alterar
                     
-                    let escolheu = Number(mudar_arr);
-                    console.log("\nIdade escolhido: "+arr[escolheu-1].idade);
+                    let escolheu = Number(escolha);
+                    console.log("\nIdade escolhida: "+arr[escolheu-1].idade);
 
-                    let mudou = input.question("Valor para mudar o Idade: ");
+                    let mudou = input.question("Nova idade: ");
                     arr[escolheu-1].idade = Number(mudou);
                     
                     }
-         else if(Number(mudar_arr) == 2){
-                
-            for(let dados3 in arr){
-                console.log(arr[dados3]);
-            };
-            mudar_arr = input.question("\nQual Nome voce deseja alterar?: "); //input para escolher qual Nome vai alterar pelo ID
+         else if(Number(escolha) == 2){
+            mostrarArr();
+            escolha = input.question("\nQual Nome voce deseja alterar?: "); //input para escolher qual Nome vai alterar pelo ID
             
-            let escolheu = Number(mudar_arr);
+            let escolheu = Number(escolha);
             console.log("\nNome escolhido: "+arr[escolheu-1].nome);
 
             let mudou = input.question("Valor para mudar o Nome: ");
             arr[escolheu-1].nome = mudou;
             
-            for(let dados in arr){
-                console.log(arr[dados]);
-            };
+        }
+        else if(Number(escolha) == 3){
+            
+            let nome = input.question("\nDigite um nome: ");
+            let idade = input.question("\nAgora uma idade: ");
+            
+            let escolheu_nome = nome;
+            let escolheu_idade = Number(idade);
+            
+
+            arr.nome+=escolheu_nome;
+            arr.idade+=escolheu_idade;
+
         }
 
-            else if( Number(mudar_arr) == 3) {
+            else if( Number(escolha) == 4) {
             console.log("Saiu!");
         }
     else{
         console.log("Opção invalida.")
     };
+    
+    mostrarArr();
