b9d465aeac30915c2c78115fa4a7d801d2d5dcd4
César Gaiotto <cesaraugusto.gaiotto@gmail.com>
2024-02-26 21:19:45 -0300

    explicando o método .match()

--- JavaScript/teste.js
+++ JavaScript/teste.js
@@ -1,23 +1,10 @@
-function XO(str){
-var str_low = str.toLowerCase();
-
-var x = str_low.indexOf("x");
-var o = str_low.indexOf("o");
-
-var cnt_x = 0;
-var cnt_o = 0;
-
-  for(var i=0; i < str_low.length; i++ ){
-    if (str_low[i] === "x"){
-      cnt_x++
-    }
-    if(str_low[i] === "o"){
-      cnt_o++
-    }
-  }
-
+function XO(str) {
+  let x = str.match(/x/gi); //o método .match() é usado para retornar uma nova array que o valor será uma string com expressão regular baseada no parâmetro, no caso vai retornar 
+  //todos valores = 'x', indenpententemente se é maiusculo ou minusculo e sem se importar onde está o valor, isso acontece por conta das sintaxes 'g' e 'i' 
+  //onde 'i' é para não ter esse "Case Sensitive" e o 'g' é para pegar todos além do primeiro valor
+  let o = str.match(/o/gi);
+  return (x && x.length) === (o && o.length);
 };
 
 console.log(XO("zzoo"));
-
 // https://www.codewars.com/kata/55908aad6620c066bc00002a/train/javascript
