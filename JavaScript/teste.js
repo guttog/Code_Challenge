40376e41fd60a40e9393ece82eecc3ce68f67791
CESAR AUGUSTO ALVES GAIOTTO <Z331982@nt.embratel.com.br>
2024-02-29 10:24:16 -0300

    adicionado o desafio O&X

--- JavaScript/teste.js
+++ JavaScript/teste.js
@@ -1,10 +1,4 @@
-function XO(str) {
-  let x = str.match(/x/gi); //o método .match() é usado para retornar uma nova array que o valor será uma string com expressão regular baseada no parâmetro, no caso vai retornar 
-  //todos valores = 'x', indenpententemente se é maiusculo ou minusculo e sem se importar onde está o valor, isso acontece por conta das sintaxes 'g' e 'i' 
-  //onde 'i' é para não ter esse "Case Sensitive" e o 'g' é para pegar todos além do primeiro valor
-  let o = str.match(/o/gi);
-  return (x && x.length) === (o && o.length);
-};
-
-console.log(XO("zzoo"));
-// https://www.codewars.com/kata/55908aad6620c066bc00002a/train/javascript
+let texto = `Um texto com 
+algumas linhas
+quebradas`;
+console.log(texto)
