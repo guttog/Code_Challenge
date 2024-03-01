22744cdde184335100a0ca423d274b02dfe68740
guttog <cesaraugusto.gaiotto@gmail.com>
2024-03-01 15:07:04 -0300

    feito o desafio getMiddle. OBS: Falta colocar explicação

---
+++ JavaScript/getMiddle.js
@@ -0,0 +1,16 @@
+function getMiddle(s)
+{
+  let resultado = s.length / 2;
+  let arredondar = Math.round(resultado);
+
+    if(s.length % 2 === 0){
+        return s[resultado-1]+""+s[resultado]
+    }
+    else{
+        return s[arredondar-1]
+    }
+}
+//LEMBRETE: Esse desafio foi para encontrar as letrar do meio da string, indenpendente se é par ou impar
+console.log(getMiddle("test"));
+console.log(getMiddle("testing"));
+console.log(getMiddle("middle"));
