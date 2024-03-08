c9a9e46d9cfae527b9ca95212e53f1083b071e42
guttog <cesaraugusto.gaiotto@gmail.com>
2024-03-08 17:12:21 -0300

    VER ESSE DASAFIO QUANDO CHEGAR EM CASAgit add .

---
+++ JavaScript/teste.js
@@ -0,0 +1,13 @@
+const str = "Let's travel abroad shall we";
+
+var arr = str.split(" ");
+
+for(var i = 0; i < arr.length; i++){
+    var menor_tamanho = Math.min(arr[i].length)
+
+}
+if(arr[i].length == menor_tamanho){
+    console.log("É o menor")
+}
+
+// https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9/train/javascript VER AGORA!!
